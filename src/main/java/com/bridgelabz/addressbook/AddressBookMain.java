5521eef815ff0de66463c05e5b9fa979047fa35c
Vaidehi-Kokare <vaidehikokare@gmail.com>
2023-02-03 17:09:08 +0530

    Added:Add Details using console

---
+++ src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.addressbook;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program");
+        AddressBook addressBook1 = new AddressBook();
+        addressBook1.addContact();
+    }
+}
